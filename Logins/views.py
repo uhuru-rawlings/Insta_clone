84b4f51aaeb1509593265286e69b9aeb75bfcb9a
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-03-04 23:50:43 +0300

    changed redirect link with set cookies

--- Logins/views.py
+++ Logins/views.py
@@ -3,19 +3,18 @@ from Signups.models import Signups
 # Create your views here.
 def login_view(request):
     errors = ""
-    try:
-        request.COOKIES['loged_in_user']
-        return redirect("/home/")
-    except:
-        pass
+    # try:
+    #     request.COOKIES['loged_in_user']
+    #     return redirect("/home/")
+    # except:
+    #     pass
     if request.method == 'POST':
         useremails = request.POST['useremails']
         passsword = request.POST['passwords']
         if Signups.objects.filter(useremail = useremails).exists():
             get_user = Signups.objects.get(useremail = useremails)
             if(get_user.password == passsword):
-                response = render(request, "index.html")
-                # response = HttpResponse()
+                response = redirect("/home/")
                 response.set_cookie("loged_in_user",useremails)
                 return response
                 # return redirect("home/")

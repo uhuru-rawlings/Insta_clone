0bd87b0bcab71afb3b05edf28f3161fcc35f5569
uhuru rawlings <rawlings.huru@student.moringaschool.com>
2022-03-04 23:20:09 +0300

    added send mail functionality

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -139,3 +139,10 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'techrollblogs@gmail.com'
+EMAIL_HOST_PASSWORD = 'raw=lings1998'
+EMAIL_HIST_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
--- Signups/views.py
+++ Signups/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Signups
+from django.core.mail import send_mail
 # Create your views here.
 def signup_view(request):
     if request.method == 'POST':
@@ -10,6 +11,8 @@ def signup_view(request):
         
         new_user = Signups(username=useraname, useremail=useremail, userimage = userimage, password= password)
         new_user.save()
+        mail_body = "Thank you for signing up to insta. You are receiving this email because your email has been used to signup to insta."
+        send_mail("Signup Notification", mail_body, "techrollblogs@gmail.com", [useremail],fail_silently=False)
         return redirect("/")
     context = {
         "title" : "Instagram - Signup"
